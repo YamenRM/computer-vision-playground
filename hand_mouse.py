0d46f1f8c1b103a0f60aac102278c9747abef09b
YamenRM <yamenrafat132@gmail.com>
2026-02-27 13:38:15 +0200

    adjusting the hand tracking options

--- hand_mouse.py
+++ hand_mouse.py
@@ -32,9 +32,9 @@ def hand_mouse(placeholder):
     options = HandLandmarkerOptions(
         base_options=base_options,
         num_hands=1,  
-        min_hand_detection_confidence=0.5, 
-        min_hand_presence_confidence=0.5,
-        min_tracking_confidence=0.5,
+        min_hand_detection_confidence=0.7, 
+        min_hand_presence_confidence=0.7,
+        min_tracking_confidence=0.9,
         running_mode=VisionRunningMode.LIVE_STREAM,
         result_callback=result_callback
     )
