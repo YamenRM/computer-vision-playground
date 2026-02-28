a89aea6d58104acbbb09ba02a038e18daafbee8f
YamenRM <yamenrafat132@gmail.com>
2026-02-28 15:28:09 +0200

    final adjustment

--- src/drawing_mode.py
+++ src/drawing_mode.py
@@ -13,7 +13,6 @@ draw_color = (0 , 255 , 255)
 
 erase_color = (0 , 0 , 0)
 
-
 if 'canvas' not in st.session_state:
     st.session_state.canvas = np.zeros((480, 640, 3), dtype=np.uint8)
 
@@ -70,7 +69,7 @@ def drawing_mode(placeholder):
             timestamp = int(time.time_ns() // 1000000)  
             landmarker.detect_async(mp_image, timestamp)
 
-            # draw the custom hand landmarks on the frame
+            # drawing logic
             if latest_result and latest_result.hand_landmarks:
                 for hand_landmarks in latest_result.hand_landmarks:
                 
@@ -79,18 +78,23 @@ def drawing_mode(placeholder):
                     middle_tip = hand_landmarks[12]
                     middle_mcp = hand_landmarks[10]
                     ring_tip = hand_landmarks[16]
-                    ring_mcp = hand_landmarks[14] # The joint below the tip
+                    ring_mcp = hand_landmarks[14]
+
 
                     curr_x, curr_y = int(index_tip.x * 640), int(index_tip.y * 480)
             
-            # 1. Check if the hand is "Open" (Ring finger tip is above its joint)
-            # Remember: In images, Y decreases as you go UP.
 
-                    if  ring_tip.y < ring_mcp.y  and index_tip.y < index_mcp.y and middle_tip.y < middle_mcp.y:
-                # --- NEUTRAL MODE ---
-                        cv2.putText(frame, "NEUTRAL (Hand Closed)", (50, 50), 
+                    if  ring_tip.y > ring_mcp.y  and index_tip.y > index_mcp.y and middle_tip.y > middle_mcp.y:
+                #  NEUTRAL MODE 
+                        cv2.putText(frame, "NEUTRAL", (50, 50), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                         last_x, last_y = 0, 0 # Reset tracking so it doesn't jump when you reopen
+                    
+                    elif ring_tip.y < ring_mcp.y  and index_tip.y < index_mcp.y and middle_tip.y < middle_mcp.y:
+                        st.session_state.canvas = np.zeros((480, 640, 3), dtype=np.uint8)
+                        last_x, last_y = 0, 0
+                        cv2.putText(frame, "CLEAR", (50, 50), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 
                     else:
                 # 2. Hand is open, now decide between DRAW and ERASE
@@ -99,14 +103,14 @@ def drawing_mode(placeholder):
                         if dist < 0.08:
                     # --- ERASING MODE ---
                             cv2.circle(frame, (curr_x, curr_y), 40, (255, 255, 255), 2)
-                            cv2.putText(frame, "ERASING", (curr_x + 35, curr_y), 
+                            cv2.putText(frame, "ERASING", (curr_x + 35, curr_y + 35), 
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
                             cv2.circle(st.session_state.canvas, (curr_x, curr_y), 40, erase_color, -1)
                             last_x, last_y = 0, 0 
                         else: 
                     # --- DRAWING MODE ---
                             cv2.circle(frame, (curr_x, curr_y), 8, draw_color, -1)
-                            cv2.putText(frame, "DRAWING", (curr_x + 15, curr_y), 
+                            cv2.putText(frame, "DRAWING", (curr_x + 15, curr_y + 15), 
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, draw_color, 1)
 
                             if last_x != 0 and last_y != 0:
